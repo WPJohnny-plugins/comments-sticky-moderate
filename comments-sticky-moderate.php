9e20b1d4bc49b532508d5131d3dd016057f3848d
hassanhashmey <havelian123@gmail.com>
2025-05-14 17:02:14 +0500

    update version to 1.1.1

--- comments-sticky-moderate.php
+++ comments-sticky-moderate.php
@@ -1,33 +1,34 @@
 <?php
 /**
  * Plugin Name: Comments Sticky Moderate
- * Description: Moves moderation options at top of each comment to speed up manual approval.
- * Version: 1.0.0
- * Author: WPJohnny
- * Author URI: https://wpjohnny.com
+ * Description: Moves moderation options to the top of each comment to speed up moderation. You can leave your mouse in one place and keep clicking, manually pruning your comments quicker and (hopefully) save false positives.
+ * Plugin URI: https://wpjohnny.com/comments-sticky-moderate/
+ * Version: 1.1.1
+ * Author: <a href="https://wpjohnny.com">WPJohnny</a>, <a href="https://profiles.wordpress.org/zeroneit/">zerOneIT</a>
  * Donate link: https://www.paypal.me/wpjohnny
- * Text Domain: comments-sticky-moderate
+ * License:      GPL v2 or later
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 defined( 'ABSPATH' ) || exit;
 
-add_action( 'admin_footer-edit-comments.php', 'csm_print_script' );
+add_action( 'admin_footer-edit-comments.php', 'csmPrintScript' );
 /**
  * Print the script.
  *
  * @since 1.0.0
  * @return void
  */
-function csm_print_script() {
+function csmPrintScript() {
 	?>
 	<script type="text/javascript">
 		jQuery(document).ready(function() {
 			jQuery('.wp-list-table .comment').each(function() {
 				var row                 = jQuery(this),
-					row_column_comment  = row.find('.column-comment'),
-					comment_row_actions = row_column_comment.find('.row-actions').detach();
+					columnCommentRow  = row.find('.column-comment'),
+					commentRowActions = columnCommentRow.find('.row-actions').detach();
 
-				comment_row_actions.prependTo(row_column_comment);
+				commentRowActions.prependTo(columnCommentRow);
 			});
 		});
 	</script>
--- readme.txt
+++ readme.txt
@@ -1,17 +1,17 @@
 === Comments Sticky Moderate ===
-Contributors: treeflips
+Contributors: treeflips, zeroneit
 Donate link: https://www.paypal.me/wpjohnny
 Tags: comments, moderate
 Requires at least: 4.0
-Tested up to: 5.5
-Stable tag: 1.0.0
+Tested up to: 6.8
+Stable tag: 1.1.1
 License: GPL-2.0+
 
-Moves moderation options to top of each comment to speed up manual approval.
+Moves moderation options to top of each comment to speed up moderation.
 
 == Description ==
 
-By having the moderation options bar on top of each comment, you can leave your mouse in one place and keep clicking. It allows you to manually prune your comments quicker and (hopefully) save false positives.
+Moves moderation options to the top of each comment to speed up moderation. You can leave your mouse in one place and keep clicking, manually pruning your comments quicker and (hopefully) save false positives.
 
 == Installation ==
 <ol>
@@ -27,9 +27,13 @@ Yes, this plugin works perfect with the latest version of WordPress! It also wor
 No. It's coded extremely lightweight, no extra CSS or JS script on frontend. It does only the essential function and nothing more. Very lightweight. No heavy PHP processing or database queries. I'm an absolute speed fanatic.
 
 == Screenshots ==
-1. Moderation options on top instead of below the comment. Now you can leave the mouse in one place and keep clicking "Delete".
+1. Moderation options on top instead of below the comment. Now you can leave the mouse in one place and keep clicking "Spam".
 
 == Changelog ==
+
+= 1.1.1 =
+- Updated for compatibility with latest WordPress version.
+- Description was updated.
 
 = 1.0.0 =
 Initial release
